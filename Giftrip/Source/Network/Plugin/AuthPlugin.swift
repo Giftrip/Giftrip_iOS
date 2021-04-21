963507965dd5c2d345208e1eb7a9639a2a3ef529
KangMinseok <qbq5000@gmail.com>
2021-04-21 16:04:37 +0900

    ✨ AuthPlugin 코드 추가

--- Giftrip/Source/Network/Plugin/AuthPlugin.swift
+++ Giftrip/Source/Network/Plugin/AuthPlugin.swift
@@ -7,6 +7,28 @@
 
 import Moya
 
-final class AuthPlugin: PluginType {
+protocol AuthorizedTargetType: TargetType {
+    var needsAuth: Bool { get }
+}
+
+struct AuthPlugin: PluginType {
+    fileprivate let authService: AuthService
     
+    init(authService: AuthService) {
+        self.authService = authService
+    }
+    
+    func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
+        guard
+            let token = authService.currentAccessToken?.token,
+            let target = target as? AuthorizedTargetType,
+            target.needsAuth
+        else {
+            return request
+        }
+        
+        var request = request
+        request.addValue("Bearer " + token, forHTTPHeaderField: "Authorization")
+        return request
+    }
 }
